4ae79440657f36095c11f7184e2f419958198541
unknown <kevinskor2000@gmail.com>
2020-05-02 22:23:09 -0600

    tarea

--- private/Config/Config.php
+++ private/Config/Config.php
@@ -1,6 +1,6 @@
 <?php
 
 include('../../Conexion/DB.php');
-$conexion = new Conexion('127.0.0.1 ','root','','db_sistemas_peliculas');
+$conexion = new Conexion('127.0.0.1','root','','db_sistema_peliculas');
 
 ?>
